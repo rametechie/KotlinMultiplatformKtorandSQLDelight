3bb76ccb39154e14b35482da75e0abeef31b9526
ramesh <rameshgetit@gmail.com>
2025-05-21 21:03:47 +0200

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/jetbrains/spacetutorial/cache/Database.kt
@@ -0,0 +1,12 @@
+package com.jetbrains.spacetutorial.cache
+
+import com.jetbrains.spacetutorial.entity.RocketLaunch
+
+internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = AppDatabase(databaseDriverFactory.createDriver())
+    private val dbQuery = database.appDatabaseQueries
+
+    internal fun getAllLaunches(): List<RocketLaunch> {
+        return dbQuery.selectAllLaunchesInfo(::mapLaunchSelecting).executeAsList()
+    }
+}
