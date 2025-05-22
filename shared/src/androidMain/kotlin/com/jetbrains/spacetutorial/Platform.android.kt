53d7d782b246ffa4a4385679c6c9bc67ea52d95c
ramesh <rameshgetit@gmail.com>
2025-05-22 20:28:34 +0200

    added factories for platform database drivers and an AppDatabase interface to perform database operations

--- shared/src/androidMain/kotlin/com/jetbrains/spacetutorial/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/jetbrains/spacetutorial/Platform.android.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.spacetutorial
 
 import android.os.Build
+import io.ktor.util.Platform
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
