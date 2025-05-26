c8be69be86dd338b1adb9827b60c7f6486db9a86
ramesh <rameshgetit@gmail.com>
2025-05-26 21:34:39 +0200

    Implement the UI and Load Data

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,14 @@
 import SwiftUI
+import Shared
 
 @main
 struct iOSApp: App {
+    init() {
+        KoinHelperKt.doInitKoin()
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: .init())
         }
     }
 }
